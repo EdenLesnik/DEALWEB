e48f4f3ace3edc507f16cedf1bf18a7b6653ddb3
EdenLesnik <lookcage@gmail.com>
2025-04-16 20:00:28 +0300

    rebuilt static files for Render

--- components/layout/PageHead.js
+++ components/layout/PageHead.js
@@ -5,7 +5,7 @@ const PageHead = ({ headTitle }) => {
         <>
             <Head>
                 <title>
-                    {headTitle ? headTitle : "Taxix | Taxi Business React Nextjs Template"}
+                    {headTitle ? headTitle : "DealTaxi"}
                 </title>
                 <link
                     href="https://fonts.googleapis.com/css2?family=Archivo:wght@300;400;500;600;700;800;900&family=Epilogue:wght@300;400;500;600;700;800;900&family=Odibee+Sans&display=swap"
